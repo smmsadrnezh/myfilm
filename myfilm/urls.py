67c4920cfebd92b1e32225d4ef1e93ebcc89157e
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-04-28 03:59:38 +0430

    add homepage

--- myfilm/urls.py
+++ myfilm/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
 
     url(r'^movie/$', 'movies.views.movie', name='salam'),
-
+    url(r'^$', 'myfilm.views.home', name='salam'),
     url(r'^admin/', include(admin.site.urls)),
 ]
