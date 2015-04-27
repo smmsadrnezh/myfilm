c18bd3a1c767cea7f868af416454509b73a2b07e
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-04-28 03:19:35 +0430

    change proj name

--- myfilm/urls.py
+++ myfilm/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
+
     url(r'^movie/$', 'myfilm.views.movie', name='salam'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
