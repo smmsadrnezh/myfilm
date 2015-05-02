136e8ed311a26f99ebf09d50899ed8b42eadf53c
arman71 <arman_naeimian@yahoo.com>
2015-05-02 18:55:00 +0430

    last version

--- myfilm/urls.py
+++ myfilm/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 
 urlpatterns = [
 
-    url(r'^movie/$', 'movies.views.movie', name='salam'),
+    url(r'^movies/1$', 'movies.views.movie', name='salam'),
     url(r'^search/$', 'search.views.search', name='salam'),
+    url(r'^artist/$', 'artists.views.artists', name='salam'),
+    url(r'^artist/TomHanks/$', 'artists.views.tomhanks', name='salam'),
     url(r'^accounts/login/$', 'accounts.views.login', name='salam'),
     url(r'^accounts/users/$', 'accounts.views.users', name='salam'),
-    url(r'^accounts/register$', 'accounts.views.register', name='salam'),
+    url(r'^accounts/register/$', 'accounts.views.register', name='salam'),
     url(r'^$', 'myfilm.views.home', name='salam'),
     url(r'^timeline/$', 'myfilm.views.timelinehome', name='salam'),
     url(r'^posts/$', 'posts.views.post', name='salam'),
