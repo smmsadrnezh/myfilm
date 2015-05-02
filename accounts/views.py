31f33cd65709ea76a9334b7640a2fab1e3b28f02
arman71 <arman_naeimian@yahoo.com>
2015-05-02 23:17:39 +0430

    html completed

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ def users(request):
     name = "Arman"
     template = get_template('users.html')
     pageHtml = template.render(Context({'name': name}))
-    template = get_template('layout.html')
+    template = get_template('tlayout.html')
     html = template.render(Context({'pageBody': pageHtml}))
     return HttpResponse(html)
 
@@ -35,7 +35,7 @@ def masoud(request):
     name = "Masoud"
     t = get_template('masoud.html')
     pageHtml = t.render(Context({'mtitle': name}))
-    t = get_template('layout.html')
+    t = get_template('tlayout.html')
     html = t.render(Context({'pageBody': pageHtml}))
     return HttpResponse(html)
 
