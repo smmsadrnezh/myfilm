cfd5a52df724378682d4cccbc24db13cd13c7e8f
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-05-03 06:45:22 +0430

    fixing navbar

--- myfilm/views.py
+++ myfilm/views.py
@@ -4,18 +4,20 @@ from django.template import Context
 # Create your views here.
 
 def home(request):
+    title = "Myfilm - Home"
     t = get_template('home.html')
     pageHtml = t.render()
     t = get_template('layout.html')
-    html = t.render(Context({'pageBody': pageHtml}))
+    html = t.render(Context({'pageBody': pageHtml, 'PageTitle': title}))
     return HttpResponse(html)
 
 
 def timelinehome(request):
+    title = "Myfilm - Timeline"
     t = get_template('post.html')
     pageHtml = t.render()
     t = get_template('timeline.html')
     pageHtml = t.render(Context({'pageBody': pageHtml}))
     t = get_template('tlayout.html')
-    html = t.render(Context({'pageBody': pageHtml}))
+    html = t.render(Context({'pageBody': pageHtml , 'PageTitle': title}))
     return HttpResponse(html)
