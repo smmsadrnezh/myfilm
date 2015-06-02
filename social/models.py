43bf8f6f85fb01978c2aef8b5b21b012caf0b3a1
arman71 <arman_naeimian@yahoo.com>
2015-06-02 19:17:43 +0430

    last

--- social/models.py
+++ social/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from myfilm.models import Movie
+from accounts.models import User
 # Create your models here.
 class Post:
     post_id = models.IntegerField(primary_key=True)
     title = models.CharField()
-    # username = models.ForeignKey(User)
-    # movie = models.ForeignKey(Movie)
+    #username = models.ForeignKey(accounts.User)
+    #movie = models.ForeignKey(myfilm.Movie)
     body = models.TextField()
     time = models.TimeField()
