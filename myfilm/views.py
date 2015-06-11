8651125e4c5962213932347ec537e48f2cff6967
arman71 <arman_naeimian@yahoo.com>
2015-06-11 13:21:47 +0430

    movie page is ok

--- myfilm/views.py
+++ myfilm/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from myfilm.models import Artist
 from myfilm.models import Movie
 from social.models import MovieRating
+from myfilm.models import MovieArtist
 
 def home(request):
     return render(request, 'home.html', {
@@ -10,6 +11,9 @@ def home(request):
 
 
 def movie(request,movietitle):
+
+    ###calculating movie rate
+
     cur_movie = Movie.objects.filter(title=movietitle)[0]
     cur_movie_voters = MovieRating.objects.filter(movie_id=cur_movie.id)
     total_rate = 0
@@ -17,8 +21,18 @@ def movie(request,movietitle):
         total_rate += voter.rate
     if total_rate!=0:
         total_rate /= len(cur_movie_voters)
+
+    ### end of calculating movie rate
+
+    ### fetch movie artists
+
+    director = MovieArtist.objects.filter(movie_id=cur_movie.id,role='director')[0]
+    writer = MovieArtist.objects.filter(movie_id=cur_movie.id,role='writer')[0]
+    stars = MovieArtist.objects.filter(movie_id=cur_movie.id,role='actor')
+    ### end fetching artists
+
     return render(request, 'movie.html', {
-        'rate':total_rate,'movie':cur_movie
+        'rate':total_rate,'movie':cur_movie,'director':director,'writer':writer,'stars':stars
     })
 
 
