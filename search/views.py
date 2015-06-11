e1cfb1b452bceed1922e2a6a090ae9940f9745fb
arman71 <arman_naeimian@yahoo.com>
2015-06-11 17:48:49 +0430

    last

--- search/views.py
+++ search/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from .forms import SearchForm
+from accounts.models import User
+from myfilm.models import Movie
 
 def search(request):
+    if request.method=='Post':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+        movies = Movie.objects.filter(title=title)
+        users = User.objects.filter(username = title)
+        movies="fight club"
+        return render(request, 'search.html', {
+            'movie': movies, 'user' : users
+        })
     return render(request, 'search.html', {
-        'PageTitle': "Search",
+
     })
