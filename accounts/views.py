bbb6398df055ef59e3851c75992aa81e3a34291d
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-06-12 14:44:12 +0430

    problem

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 
 
 def login(request):
-    return render(request, 'login.html', {
-        'PageTitle': "Login",
-    })
+    c = {}
+    c.update(csrf(request))
+    return render(request,'login.html', dict(c, **{'PageTitle': "Login"}))
 
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username,password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('/timeline')
+    else:
+        return HttpResponseRedirect('/invalid')
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect('/home')
 
 def accounts_lists(request):
     return render(request, 'users.html', {
