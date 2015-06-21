b48bab600e42b6fc357c28198961d04b548cac38
arman71 <arman_naeimian@yahoo.com>
2015-06-21 20:03:22 +0430

    captcha error

--- myfilm/urls.py
+++ myfilm/urls.py
@@ -38,6 +38,6 @@ urlpatterns = [
                   url(r'^admin/', include(admin.site.urls)),
 
                   # captcha url
-                  url(r'^captcha/', include('captcha.urls')),
+                  #url(r'^captcha/', include('captcha.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
