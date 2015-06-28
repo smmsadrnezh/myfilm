f5f531f9810e115e0d15099798fc35b46fca3e56
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-06-28 16:12:31 +0430

    datepicker

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,6 +10,7 @@ class CustomRegistration(UserCreationForm):
     email = forms.EmailField(required=True,widget=forms.TextInput(attrs={'class': 'form-control' , 'placeholder':'enter your email'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control' , 'placeholder':'enter your first name'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'  , 'placeholder':'enter your last name'}))
+    birth_date = forms.DateField(widget=forms.TextInput(attrs={'class': 'form-control datepicker'  , 'placeholder':'enter your birthdate'}))
     captcha = CaptchaField()
 
     class Meta:
