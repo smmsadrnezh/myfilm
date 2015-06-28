7e9740f443a958613e9cf0bc6aac9ab90568be67
Masoud Sadrnezhaad <smmsadrnezh@gmail.com>
2015-06-28 08:03:19 +0430

    ajax search and refactoring

--- search/views.py
+++ search/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+
 from accounts.models import CustomUser
 from myfilm.models import Movie
 import social.views
@@ -11,8 +12,9 @@ import accounts.views
 @login_required
 def search(request):
     if request.method == 'POST':
-        return render(request, 'search.html', {
+        return render(request, 'result_page.html', {
             'pageTitle': " - Search",
+            'search_string': request.POST.get('search_string'),
             'movies_result': Movie.objects.filter(title__contains=request.POST.get('search_string')),
             'users_result': CustomUser.objects.filter(username__contains=request.POST.get('search_string')),
             'who_to_follows': social.views.who_to_follow(request),
@@ -23,3 +25,11 @@ def search(request):
         })
     else:
         return HttpResponseRedirect('/timeline')
+
+
+def ajax_search(request, search_string):
+    return HttpResponse(render(request, 'result_ajax.html', {
+        'search_string': search_string,
+        'movies_search_results': CustomUser.objects.filter(username__contains=request.POST.get('search_string')),
+        'users_search_results': CustomUser.objects.filter(username__contains=request.POST.get('search_string')),
+    }))
